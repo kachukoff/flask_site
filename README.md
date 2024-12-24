d7ac27a96449d4c0abc267582849aae17d733001
Kachukov Alexandr <kachukoff2014@yandex.ru>
2024-12-24 13:18:24 +0300

    Step4

---
+++ README.md
@@ -0,0 +1,9 @@
+# 🐍🥤 Cоздаем адаптивный сайт для GitHub Pages
+
+[Flask за час. Часть 1: создаем адаптивный сайт для GitHub Pages](https://proglib.io/p/flask-za-chas-chast-1-sozdaem-adaptivnyy-sayt-dlya-github-pages-2022-06-20)
+
+[Flask за час. Часть 2: завершаем разработку и размещаем сайт на GitHub Pages](https://proglib.io/p/flask-za-chas-chast-2-zavershaem-razrabotku-i-razmeshchaem-sayt-na-github-pages-2022-06-22)
+
+[Готовый сайт](https://natkaida.github.io/flask_site/)
+
+![site](https://user-images.githubusercontent.com/85797091/176396130-943941d7-a5de-4828-af88-c5ed2657032f.jpg)
--- mysite.py
+++ mysite.py
@@ -8,6 +8,7 @@ FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 FLATPAGES_ROOT = 'content'
 POST_DIR = 'posts'
+PORT_DIR = 'portfolio'
 app = Flask(__name__)
 flatpages = FlatPages(app)
 freezer = Freezer(app)
@@ -17,16 +18,38 @@ app.config.from_object(__name__)
 def index():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
     posts.sort(key=lambda item: item['date'], reverse=True)
+    cards = [p for p in flatpages if p.path.startswith(PORT_DIR)]
+    cards.sort(key=lambda item: item['title'])    
     with open('settings.txt', encoding='utf8') as config:
         data = config.read()
         settings = json.loads(data)
-    return render_template('index.html', posts=posts, bigheader=True, **settings)
+    tags = set()
+    for p in flatpages:
+        t = p.meta.get('tag')
+        if t:
+            tags.add(t.lower())
+
+    return render_template('index.html', posts=posts, cards=cards, bigheader=True, **settings, tags=tags)
 
 @app.route('/posts/<name>/')
 def post(name):
     path = '{}/{}'.format(POST_DIR, name)
     post = flatpages.get_or_404(path)
     return render_template('post.html', post=post)
+
+@app.route('/portfolio/<name>/')
+def card(name):
+    path = '{}/{}'.format(PORT_DIR, name)
+    card = flatpages.get_or_404(path)
+    return render_template('card.html', card=card)    
+
+@app.route('/pygments.css')
+def pygments_css():
+    return pygments_style_defs('monokai'), 200, {'Content-Type': 'text/css'}    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
